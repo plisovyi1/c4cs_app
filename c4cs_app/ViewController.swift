12e3dd691293f96d1169345676e9a2d83213dcfc
Lisovyi, Pavlo <plisovyi@nw21502.ccs.itd.umich.edu>
2017-04-06 12:53:40 -0400

    Initial Commit

---
+++ c4cs_app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  c4cs_app
+//
+//  Created by Lisovyi, Pavlo on 4/6/17.
+//  Copyright © 2017 Lake. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
